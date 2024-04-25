53e6901dcc6ac5682e43408ce8f01561b450a65e
themiyakasun <themiyakasun.tb@gmail.com>
2024-04-25 11:21:09 +0530

    About section

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php include 'includes/header.php'; ?>
 
+<section class="about-section">
+ <div class="container">
+  <div class="about-wrapper">
+   <div class="about-image">
+    <img src="assets/images/about.jpg" alt="About Us">
+   </div>
+   <div class="about-text">
+    <h2>About Us</h2>
+    <h1>We Believe in Change</h1>
+    <p class="mb-3">STEMUp-NSBM is a student-led organization that aims to promote STEM education among the youth in Sri Lanka. We believe that STEM literacy is a foundation skill that matters for all careers. STEM builds critical thinking abilities, creativity and collaboration skills. Our mission is to inspire and empower the next generation of innovators and problem solvers.</p>
+    <a href="#" class="button button-primary">More about us</a>
+   </div>
+  </div>
+ </div>
+</section>
 
+<section class="events-section">
+ <div class="container"></div>
+</section>
 
 
 <?php include 'includes/footer.php';?>
