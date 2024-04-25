d1566b0d94505de06b8739b847b9c6d15b60f069
Kaveeshe Waduge <109468529+Kaveeshe2001@users.noreply.github.com>
2024-04-25 15:24:43 +0530

    Add card

--- blog.php
+++ blog.php
@@ -20,7 +20,84 @@
     <div class="container">
         <div class="row">
             <div class="col-8">
-                Example
+                <div class="row mt-5">
+                    <div class="col-md-4 mb-5">
+                     <a href="" class="card">
+                        <div class="card-top">
+                         <img src="assets/images/microbit.png" class="card__image" alt="" />
+                        </div>
+                        <div class="card__overlay">
+                          <div class="card__header">
+                            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+                            <div class="card__header-text">
+                              <h3 class="card__title">Micro:bit SLUG</h3>            
+                            </div>
+                          </div>
+                          <p class="card__description">Micro:bit Sri Lanka from its initiation has strived to empower the community by teaching kids programming using Micro:bit devices.
+                          </p>
+                        </div>
+                     </a>
+                    </div>
+
+                    <div class="col-md-4 mb-5">
+                     <a href="" class="card">
+                        <div class="card-top">
+                         <img src="assets/images/microbit.png" class="card__image" alt="" />
+                        </div>
+                        <div class="card__overlay">
+                          <div class="card__header">
+                            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+                            <div class="card__header-text">
+                              <h3 class="card__title">Micro:bit SLUG</h3>            
+                            </div>
+                          </div>
+                          <p class="card__description">Micro:bit Sri Lanka from its initiation has strived to empower the community by teaching kids programming using Micro:bit devices.
+                          </p>
+                        </div>
+                     </a>
+                    </div>
+
+                </div>
+
+                <!--Second row-->
+                <div class="row mt-5">
+                    <div class="col-md-4 mb-5">
+                     <a href="" class="card">
+                        <div class="card-top">
+                         <img src="assets/images/microbit.png" class="card__image" alt="" />
+                        </div>
+                        <div class="card__overlay">
+                          <div class="card__header">
+                            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+                            <div class="card__header-text">
+                              <h3 class="card__title">Micro:bit SLUG</h3>            
+                            </div>
+                          </div>
+                          <p class="card__description">Micro:bit Sri Lanka from its initiation has strived to empower the community by teaching kids programming using Micro:bit devices.
+                          </p>
+                        </div>
+                     </a>
+                    </div>
+
+                    <div class="col-md-4 mb-5">
+                     <a href="" class="card">
+                        <div class="card-top">
+                         <img src="assets/images/microbit.png" class="card__image" alt="" />
+                        </div>
+                        <div class="card__overlay">
+                          <div class="card__header">
+                            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+                            <div class="card__header-text">
+                              <h3 class="card__title">Micro:bit SLUG</h3>            
+                            </div>
+                          </div>
+                          <p class="card__description">Micro:bit Sri Lanka from its initiation has strived to empower the community by teaching kids programming using Micro:bit devices.
+                          </p>
+                        </div>
+                     </a>
+                    </div>
+
+                </div>
             </div>
             <div class="col-sm-2">
                 Example
