51426b9e3991a5417682ed86a0cd9a0ed46b9d7e
Kaveeshe Waduge <109468529+Kaveeshe2001@users.noreply.github.com>
2024-04-25 13:59:00 +0530

    Create hero image of blog page

---
+++ blog.php
@@ -0,0 +1,15 @@
+<?php include 'includes/head.php'; ?>
+
+<body>
+<?php include 'includes/navbar.php'; ?>
+
+<div class="container">
+    <div class="hero-blog">
+    </div>
+    <div class="hero-blog-content">
+        <h1>Our Blog</h1>
+        <p><span class="home">Home</span> / <span>Blog</span></p>
+    </div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
