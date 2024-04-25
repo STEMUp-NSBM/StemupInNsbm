8f181b0c960a4353c6fb875ab619ccea48781733
DgvDilshan <137377400+DgvDilshan@users.noreply.github.com>
2024-04-25 16:56:53 +0530

    cheking

--- Registration.php
+++ Registration.php
@@ -1,6 +1,85 @@
 <?php include 'includes/header.php'; ?>
+    <div class="container">
+        <form action="##">
+            <div class="form first">
+                 <div class="details personal">
+                    <span class="title">Personal details</span>
+
+                    <div class="fields">
+                        <div class="input-field">
+                            <label for="">Full Name</label>
+                            <input type="text" placeholder="Enter Your Name" required>
+                        </div>
+
+                        <div class="input-field">
+                          <label for="">Date of Birth</label>
+                          <input type="date" placeholder="Enter Your date of Birth" required>
+                      </div>
+
+                      <div class="input-field">
+                        <label for="">Emaill</label>
+                        <input type="email" placeholder="Enter Your Emaill" required>
+                    </div>
+
+                    <div class="input-field">
+                      <label for="">Mobile Number</label>
+                      <input type="number" placeholder="Enter Your Mobile Number " required>
+                  </div>
+
+                  <div class="input-field">
+                    <label for="">Gender</label>
+                    <input type="date" placeholder="Enter Your Gender" required>
+                  </div>
+
+                  <div class="input-field">
+                  <label for="">Occupation</label>
+                  <input type="email" placeholder="Enter Your Occupation " required>
+                  </div>
 
 
+                </div>
+              </div>
 
+
+              <div class="details ID">
+                <span class="title">Identity details</span>
+
+                <div class="fields">
+                    <div class="input-field">
+                        <label for="">Full Name</label>
+                        <input type="text" placeholder="Enter Your Name" required>
+                    </div>
+
+                    <div class="input-field">
+                      <label for="">Date of Birth</label>
+                      <input type="date" placeholder="Enter Your date of Birth" required>
+                  </div>
+
+                  <div class="input-field">
+                    <label for="">Emaill</label>
+                    <input type="email" placeholder="Enter Your Emaill" required>
+                </div>
+
+                <div class="input-field">
+                  <label for="">Mobile Number</label>
+                  <input type="number" placeholder="Enter Your Mobile Number " required>
+              </div>
+
+              <div class="input-field">
+                <label for="">Gender</label>
+                <input type="date" placeholder="Enter Your Gender" required>
+              </div>
+
+              <div class="input-field">
+              <label for="">Occupation</label>
+              <input type="email" placeholder="Enter Your Occupation " required>
+              </div>
+
+
+            </div>
+          </div>
+            </div>
+        </form>
+    </div>
 
 <?php include 'includes/footer.php';?>
