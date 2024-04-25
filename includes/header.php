3a1aec82e40290c2420484aa4b203cb822d1c27c
themiyakasun <themiyakasun.tb@gmail.com>
2024-04-25 10:53:30 +0530

    Hero

--- includes/header.php
+++ includes/header.php
@@ -16,5 +16,16 @@
 
 </head>
 <body>
+ <header>
+  <?php include "includes/navbar.php"; ?>
 
-<?php include "includes/navbar.php"; ?>
+  <div class="hero-text-box">
+   <h1>Welcome to STEMUp-NSBM</h1>
+   <p>STEM literacy is a foundation skill that matters for all careers.</p>
+   <p>STEM builds critical thinking abilities, creativity and collaboration skills</p>
+   <div class="hero-links">
+    <a href="#" class="button button-primary">Volunteer</a>
+    <a href="#" class="button button-secondary">STEMUp Main</a>
+   </div>
+  </div>
+ </header>
