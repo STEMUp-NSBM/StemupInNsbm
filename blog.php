5f5e5ec94cde4660bf2bc5a124925f2383b89614
Kaveeshe Waduge <109468529+Kaveeshe2001@users.noreply.github.com>
2024-04-27 21:14:35 +0530

    Update card

--- blog.php
+++ blog.php
@@ -17,119 +17,156 @@
         <h3>VIEW OUR BLOG</h3>
         <h1>List of Posts</h1>
     </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-8">
-                <div class="row mt-5">
-                    <div class="col">
-                        <div class="blog-card">
-                            <div class="blog-card-top">
-                                <img src="assets/images/blog-card1.jpg">
-                            </div>
-                            <div class="blog-card-content">
-                                <div class="blog-card-header">
-                                    <div class="blog-date">
-                                        <p><i class="fa-solid fa-calendar-days"></i> 4/24/2024</p>
-                                    </div>
-                                    <div class="blog-category">
-                                        <p><i class="fa-solid fa-tag"></i> News</p>
-                                    </div>
-                                </div>
-                                <h3><a hrref="#">Micro:bit Version 2 goes live in Sri Lanka</a></h3>
-                                <p>What would you say if I told you that the BBC made a 
-                                    computer? Well, that’s exactly what they did with the 
-                                    BBC Micro:bit. It’s a single board microcontroller, 
-                                    which can be used to teach anyone, especi
-                                </p>
-                                <a href="#">Read More...</a>
-                            </div>
-                        </div>
-                    </div>
 
-                    <div class="col">
-                        <div class="blog-card">
-                            <div class="blog-card-top">
-                                <img src="assets/images/blog-card1.jpg">
-                            </div>
-                            <div class="blog-card-content">
-                                <div class="blog-card-header">
-                                    <div class="blog-date">
-                                        <p><i class="fa-solid fa-calendar-days"></i> 4/24/2024</p>
-                                    </div>
-                                    <div class="blog-category">
-                                        <p><i class="fa-solid fa-tag"></i> News</p>
-                                    </div>
-                                </div>
-                                <h3><a hrref="#">Micro:bit Version 2 goes live in Sri Lanka</a></h3>
-                                <p>What would you say if I told you that the BBC made a 
-                                    computer? Well, that’s exactly what they did with the 
-                                    BBC Micro:bit. It’s a single board microcontroller, 
-                                    which can be used to teach anyone, especi
-                                </p>
-                                <a href="#">Read More...</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row mt-5">
-                    <div class="col">
-                        <div class="blog-card">
-                            <div class="blog-card-top">
-                                <img src="assets/images/blog-card1.jpg">
-                            </div>
-                            <div class="blog-card-content">
-                                <div class="blog-card-header">
-                                    <div class="blog-date">
-                                        <p><i class="fa-solid fa-calendar-days"></i> 4/24/2024</p>
-                                    </div>
-                                    <div class="blog-category">
-                                        <p><i class="fa-solid fa-tag"></i> News</p>
-                                    </div>
-                                </div>
-                                <h3><a hrref="#">Micro:bit Version 2 goes live in Sri Lanka</a></h3>
-                                <p>What would you say if I told you that the BBC made a 
-                                    computer? Well, that’s exactly what they did with the 
-                                    BBC Micro:bit. It’s a single board microcontroller, 
-                                    which can be used to teach anyone, especi
-                                </p>
-                                <a href="#">Read More...</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col">
-                        <div class="blog-card">
-                            <div class="blog-card-top">
-                                <img src="assets/images/blog-card1.jpg">
-                            </div>
-                            <div class="blog-card-content">
-                                <div class="blog-card-header">
-                                    <div class="blog-date">
-                                        <p><i class="fa-solid fa-calendar-days"></i> 4/24/2024</p>
-                                    </div>
-                                    <div class="blog-category">
-                                        <p><i class="fa-solid fa-tag"></i> News</p>
-                                    </div>
-                                </div>
-                                <h3><a hrref="#">Micro:bit Version 2 goes live in Sri Lanka</a></h3>
-                                <p>What would you say if I told you that the BBC made a 
-                                    computer? Well, that’s exactly what they did with the 
-                                    BBC Micro:bit. It’s a single board microcontroller, 
-                                    which can be used to teach anyone, especi
-                                </p>
-                                <a href="#">Read More...</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="col-4">
-                Example
-            </div>
+    <div class="blog container">
+        <!--blog-card-1-->
+    <div class="blog-box">
+        <img src="./assets/images/student.jpg" class="blog-img">
+        <p class="blog-category">News</p>
+        <a href="" class="blog-title"> Micro:bit Version 2 goes live in Sri Lanka </a>
+        <span class="blog-date">26/04/2024</span>
+        <p class="blog-description">What would you say if I told you that the BBC made a computer? Well, that’s exactly what they did with the BBC Micro:bit. It’s a single board microcontroller, which can be used to teach anyone</p>
+        <div class="blog-profile">
+            <img src="./assets/images/student.jpg" alt="" class="blog-profile-img">
+            <span class="blog-profile-name">Kaveesha Waduge</span>
         </div>
     </div>
+    <!--blog-card-2-->
+    <div class="blog-box">
+        <img src="./assets/images/student.jpg" class="blog-img">
+        <p class="blog-category">News</p>
+        <a href="" class="blog-title"> Micro:bit Version 2 goes live in Sri Lanka </a>
+        <span class="blog-date">26/04/2024</span>
+        <p class="blog-description">What would you say if I told you that the BBC made a computer? Well, that’s exactly what they did with the BBC Micro:bit. It’s a single board microcontroller, which can be used to teach anyone</p>
+        <div class="blog-profile">
+            <img src="./assets/images/student.jpg" alt="" class="blog-profile-img">
+            <span class="blog-profile-name">Kaveesha Waduge</span>
+        </div>
+    </div>
+    <!--blog-card-3-->
+    <div class="blog-box">
+        <img src="./assets/images/student.jpg" class="blog-img">
+        <p class="blog-category">News</p>
+        <a href="" class="blog-title"> Micro:bit Version 2 goes live in Sri Lanka </a>
+        <span class="blog-date">26/04/2024</span>
+        <p class="blog-description">What would you say if I told you that the BBC made a computer? Well, that’s exactly what they did with the BBC Micro:bit. It’s a single board microcontroller, which can be used to teach anyone</p>
+        <div class="blog-profile">
+            <img src="./assets/images/student.jpg" alt="" class="blog-profile-img">
+            <span class="blog-profile-name">Kaveesha Waduge</span>
+        </div>
+    </div>
+    <!--blog-card-4-->
+    <div class="blog-box">
+        <img src="./assets/images/student.jpg" class="blog-img">
+        <p class="blog-category">News</p>
+        <a href="" class="blog-title"> Micro:bit Version 2 goes live in Sri Lanka </a>
+        <span class="blog-date">26/04/2024</span>
+        <p class="blog-description">What would you say if I told you that the BBC made a computer? Well, that’s exactly what they did with the BBC Micro:bit. It’s a single board microcontroller, which can be used to teach anyone</p>
+        <div class="blog-profile">
+            <img src="./assets/images/student.jpg" alt="" class="blog-profile-img">
+            <span class="blog-profile-name">Kaveesha Waduge</span>
+        </div>
+    </div>
+    <!--Second Row-->
+    <!--blog-card-5-->
+    <div class="blog-box">
+        <img src="./assets/images/student.jpg" class="blog-img">
+        <p class="blog-category">News</p>
+        <a href="" class="blog-title"> Micro:bit Version 2 goes live in Sri Lanka </a>
+        <span class="blog-date">26/04/2024</span>
+        <p class="blog-description">What would you say if I told you that the BBC made a computer? Well, that’s exactly what they did with the BBC Micro:bit. It’s a single board microcontroller, which can be used to teach anyone</p>
+        <div class="blog-profile">
+            <img src="./assets/images/student.jpg" alt="" class="blog-profile-img">
+            <span class="blog-profile-name">Kaveesha Waduge</span>
+        </div>
+    </div>
+    <!--blog-card-6-->
+    <div class="blog-box">
+        <img src="./assets/images/student.jpg" class="blog-img">
+        <p class="blog-category">News</p>
+        <a href="" class="blog-title"> Micro:bit Version 2 goes live in Sri Lanka </a>
+        <span class="blog-date">26/04/2024</span>
+        <p class="blog-description">What would you say if I told you that the BBC made a computer? Well, that’s exactly what they did with the BBC Micro:bit. It’s a single board microcontroller, which can be used to teach anyone</p>
+        <div class="blog-profile">
+            <img src="./assets/images/student.jpg" alt="" class="blog-profile-img">
+            <span class="blog-profile-name">Kaveesha Waduge</span>
+        </div>
+    </div>
+        <!--blog-card-7-->
+        <div class="blog-box">
+        <img src="./assets/images/student.jpg" class="blog-img">
+        <p class="blog-category">News</p>
+        <a href="" class="blog-title"> Micro:bit Version 2 goes live in Sri Lanka </a>
+        <span class="blog-date">26/04/2024</span>
+        <p class="blog-description">What would you say if I told you that the BBC made a computer? Well, that’s exactly what they did with the BBC Micro:bit. It’s a single board microcontroller, which can be used to teach anyone</p>
+        <div class="blog-profile">
+            <img src="./assets/images/student.jpg" alt="" class="blog-profile-img">
+            <span class="blog-profile-name">Kaveesha Waduge</span>
+        </div>
+    </div>
+        <!--blog-card-8-->
+        <div class="blog-box">
+        <img src="./assets/images/student.jpg" class="blog-img">
+        <p class="blog-category">News</p>
+        <a href="" class="blog-title"> Micro:bit Version 2 goes live in Sri Lanka </a>
+        <span class="blog-date">26/04/2024</span>
+        <p class="blog-description">What would you say if I told you that the BBC made a computer? Well, that’s exactly what they did with the BBC Micro:bit. It’s a single board microcontroller, which can be used to teach anyone</p>
+        <div class="blog-profile">
+            <img src="./assets/images/student.jpg" alt="" class="blog-profile-img">
+            <span class="blog-profile-name">Kaveesha Waduge</span>
+        </div>
+    </div>
+    <!--Third Row-->
+    <!--blog-card-9-->
+    <div class="blog-box">
+        <img src="./assets/images/student.jpg" class="blog-img">
+        <p class="blog-category">News</p>
+        <a href="" class="blog-title"> Micro:bit Version 2 goes live in Sri Lanka </a>
+        <span class="blog-date">26/04/2024</span>
+        <p class="blog-description">What would you say if I told you that the BBC made a computer? Well, that’s exactly what they did with the BBC Micro:bit. It’s a single board microcontroller, which can be used to teach anyone</p>
+        <div class="blog-profile">
+            <img src="./assets/images/student.jpg" alt="" class="blog-profile-img">
+            <span class="blog-profile-name">Kaveesha Waduge</span>
+        </div>
+    </div>    
+    <!--blog-card-10-->
+    <div class="blog-box">
+        <img src="./assets/images/student.jpg" class="blog-img">
+        <p class="blog-category">News</p>
+        <a href="" class="blog-title"> Micro:bit Version 2 goes live in Sri Lanka </a>
+        <span class="blog-date">26/04/2024</span>
+        <p class="blog-description">What would you say if I told you that the BBC made a computer? Well, that’s exactly what they did with the BBC Micro:bit. It’s a single board microcontroller, which can be used to teach anyone</p>
+        <div class="blog-profile">
+            <img src="./assets/images/student.jpg" alt="" class="blog-profile-img">
+            <span class="blog-profile-name">Kaveesha Waduge</span>
+        </div>
+    </div>    
+    <!--blog-card-11-->
+    <div class="blog-box">
+        <img src="./assets/images/student.jpg" class="blog-img">
+        <p class="blog-category">News</p>
+        <a href="" class="blog-title"> Micro:bit Version 2 goes live in Sri Lanka </a>
+        <span class="blog-date">26/04/2024</span>
+        <p class="blog-description">What would you say if I told you that the BBC made a computer? Well, that’s exactly what they did with the BBC Micro:bit. It’s a single board microcontroller, which can be used to teach anyone</p>
+        <div class="blog-profile">
+            <img src="./assets/images/student.jpg" alt="" class="blog-profile-img">
+            <span class="blog-profile-name">Kaveesha Waduge</span>
+        </div>
+    </div>    
+    <!--blog-card-12-->
+    <div class="blog-box">
+        <img src="./assets/images/student.jpg" class="blog-img">
+        <p class="blog-category">News</p>
+        <a href="" class="blog-title"> Micro:bit Version 2 goes live in Sri Lanka </a>
+        <span class="blog-date">26/04/2024</span>
+        <p class="blog-description">What would you say if I told you that the BBC made a computer? Well, that’s exactly what they did with the BBC Micro:bit. It’s a single board microcontroller, which can be used to teach anyone</p>
+        <div class="blog-profile">
+            <img src="./assets/images/student.jpg" alt="" class="blog-profile-img">
+            <span class="blog-profile-name">Kaveesha Waduge</span>
+        </div>
+    </div>
+    </div>
 </section>
+
 
 <?php include 'includes/footer.php'; ?>
