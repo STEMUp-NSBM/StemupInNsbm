c280d8f246e59bbbf00f519a7794f13bbb09e24c
DgvDilshan <137377400+DgvDilshan@users.noreply.github.com>
2024-04-28 23:49:15 +0530

    adding the colloms in form

--- Registration.php
+++ Registration.php
@@ -47,23 +47,29 @@
 
                 <div class="fields">
                     <div class="input-field">
-                        <label for="">Full Name</label>
+                        <label for="">Student Name</label>
                         <input type="text" placeholder="Enter Your Name" required>
                     </div>
 
                     <div class="input-field">
-                      <label for="">Date of Birth</label>
-                      <input type="date" placeholder="Enter Your date of Birth" required>
+                      <label for="">Student ID</label>
+                      <input type="number" placeholder="Enter Your Student card ID" required>
                   </div>
 
                   <div class="input-field">
-                    <label for="">Emaill</label>
-                    <input type="email" placeholder="Enter Your Emaill" required>
+                    <label for="">Batch</label>
+                    <select id="Batch-id" name="Batch">
+                      <option value="1">22.1</option>
+                      <option value="1">22.2</option>
+                      <option value="1">23.1</option>
+                      <option value="1">23.2</option>
+                      <option value="1">24.1</option>
+                    </select>
                 </div>
 
                 <div class="input-field">
-                  <label for="">Mobile Number</label>
-                  <input type="number" placeholder="Enter Your Mobile Number " required>
+                  <label for="">Volunteer Experions</label>
+                  <textarea id="Experions" name="Experions" rows="4" cols="50"></textarea>
               </div>
 
               <div class="input-field">
