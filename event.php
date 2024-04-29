ea224320103ddc0ca1b12952ea1192a94f2c9320
Kaveeshe Waduge <109468529+Kaveeshe2001@users.noreply.github.com>
2024-04-29 22:16:06 +0530

    Create Hero-Event

---
+++ event.php
@@ -0,0 +1,15 @@
+<?php include 'includes/head.php'; ?>
+
+<body>
+<?php include 'includes/navbar.php'; ?>
+
+<div class="container">
+    <div class="hero-event"></div>
+    <div class="hero-event-content">
+        <h1>Our Events</h1>
+        <p><span class="home">Home</span> / <span>Events</span></p>
+    </div>
+</div>
+
+
+<?php include 'includes/footer.php'; ?>
