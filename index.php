b8c39d03de39bdafafb2faab5809183f44667ccc
themiyakasun <themiyakasun.tb@gmail.com>
2024-05-02 00:26:10 +0530

    Loader

--- index.php
+++ index.php
@@ -1,171 +1,19 @@
-<?php include 'includes/header.php'; ?>
+<?php include 'includes/head.php'; ?>
 
-<section class="about-section">
- <div class="container">
-  <div class="about-wrapper">
-   <div class="about-image">
-    <img src="assets/images/about.jpg" alt="About Us">
-   </div>
-   <div class="about-text">
-    <h2>About Us</h2>
-    <h1>We Believe in Change</h1>
-    <p class="mb-3">STEMUp-NSBM is a student-led organization that aims to promote STEM education among the youth in Sri Lanka. We believe that STEM literacy is a foundation skill that matters for all careers. STEM builds critical thinking abilities, creativity and collaboration skills. Our mission is to inspire and empower the next generation of innovators and problem solvers.</p>
-    <a href="#" class="button button-primary">More about us</a>
-   </div>
-  </div>
- </div>
-</section>
+<div class="opening">
+ <a href="home.php" id="link" class='glowing-btn'><span class='glowing-txt'>C<span class='faulty-letter'>L</span>ICK</span></a>
+</div>
+ 
 
-<section class="events-section">
- <div class="container">
-  <h1>Recent Events</h1>
- </div>
-
- <div class="container">
-  <div class="row mt-5">
-   <div class="col-md-4 mb-5">
-    <a href="" class="card">
-       <div class="card-top">
-        <img src="assets/images/microbit.png" class="card__image" alt="" />
-       </div>
-       <div class="card__overlay">
-         <div class="card__header">
-           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-           <div class="card__header-text">
-             <h3 class="card__title">Micro:bit SLUG</h3>            
-           </div>
-         </div>
-         <p class="card__description">Micro:bit Sri Lanka from its initiation has strived to empower the community by teaching kids programming using Micro:bit devices.
-         </p>
-       </div>
-    </a>
-   </div>
-
-   <div class="col-md-4 mb-5">
-    <a href="" class="card">
-       <div class="card-top">
-        <img src="assets/images/CoderDojo-Sri-Lanka-logo.png" class="card__image" alt="" />
-       </div>
-       <div class="card__overlay">
-         <div class="card__header">
-           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-           <div class="card__header-text">
-             <h3 class="card__title">CoderDojo Sri Lanka</h3>            
-           </div>
-         </div>
-         <p class="card__description">CoderDojo Sri Lanka is the Sri Lankan network of “CoderDojo” – an international movement oriented around the running of fun, free and social coding clubs for young people.
-         </p>
-       </div>
-    </a>
-   </div>
-
-   <div class="col-md-4 mb-5">
-    <a href="" class="card">
-       <div class="card-top">
-        <img src="assets/images/ML-for-Kids-Logo.png" class="card__image" alt="" />
-       </div>
-       <div class="card__overlay">
-         <div class="card__header">
-           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-           <div class="card__header-text">
-             <h3 class="card__title">Machine Learning for Kids</h3>            
-           </div>
-         </div>
-         <p class="card__description">There is a high probability of being no field left untouched by ML and AI in the future. All of which makes it very important for us to ensure that our kids are well versed in both ML & AI
-         </p>
-       </div>
-    </a>
-    </div>
-   </div>
-
-  <div class="text-center">
-   <a href="#" class="button button-primary">View all events</a>
-  </div>
- </div>
-
-</section>
-
-<section class="branch-counsellors-section">
- <div class="container">
-  <div class="counsellor-wrapper">
-   <div class="counsellor-text">
-     <h1>Student Branch Counsellors</h1>
-   </div>
-   <div class="counsellor-members">
-    <div class="row">
-     <div class="col-md-6 mb-5">
-      <div class="counsellor-card">
-       <div class="counsellor-image">
-        <img src="assets/images/counsellor.jpg" alt="Counsellor 1">
-       </div>
-       <div class="counsellor-info">
-        <h3>John Doe</h3>
-        <p>President</p>
-       </div>
-      </div>
-     </div>
-     <div class="col-md-6 mb-5">
-      <div class="counsellor-card">
-       <div class="counsellor-image">
-        <img src="assets/images/counsellor.jpg" alt="Counsellor 1">
-       </div>
-       <div class="counsellor-info">
-        <h3>John Doe</h3>
-        <p>President</p>
-       </div>
-      </div>
-     </div>
-     <div class="col-md-6 mb-5">
-      <div class="counsellor-card">
-       <div class="counsellor-image">
-        <img src="assets/images/counsellor.jpg" alt="Counsellor 1">
-       </div>
-       <div class="counsellor-info">
-        <h3>John Doe</h3>
-        <p>President</p>
-       </div>
-      </div>
-     </div>
-    </div>
-   </div>
-  </div>
- </div>
-</section>
-
-<section class="executive-members-section">
- <div class="container">
-  <h1>Meet the Executive Commitee</h1>
-
-  <div class="row mt-5">
-   <div class="col-md-4 col-sm-6" style="height: auto !important; min-height: 0px !important;">
-                <div class="our-team">
-                    <div class="team_img">
-                        <img src="assets/images/student.jpg">
-                        <ul class="social">
-                            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
-                        </ul>
-                    </div>
-                    <div class="team-content">
-                        <h3 class="title">Williamson</h3>
-                        <span class="post">President</span>
-                    </div>
-                </div>
-                <div class="google-auto-placed" style="width: 100%; height: auto; clear: both; text-align: center;"><ins data-ad-format="auto" class="adsbygoogle adsbygoogle-noablate" data-ad-client="ca-pub-3311815518700050" data-adsbygoogle-status="done" style="display: block; margin: 10px auto; background-color: transparent; height: 0px;" data-ad-status="unfilled"><div id="aswift_3_host" style="border: none; height: 0px; width: 360px; margin: 0px; padding: 0px; position: relative; visibility: visible; background-color: transparent; display: inline-block; overflow: hidden; opacity: 0;"><iframe id="aswift_3" name="aswift_3" browsingtopics="true" style="left: 0px; position: absolute; top: 0px; border: 0px; width: 360px; height: 0px;" sandbox="allow-forms allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-top-navigation-by-user-activation" width="360" height="0" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no" allow="attribution-reporting; run-ad-auction" src="https://googleads.g.doubleclick.net/pagead/ads?client=ca-pub-3311815518700050&output=html&h=100&adk=1396942652&adf=1128811123&pi=t.aa~a.692994927~rp.4&w=360&fwrn=4&fwrnh=100&lmt=1714029748&rafmt=1&to=qs&pwprc=2225554053&format=360x100&url=https%3A%2F%2Fbestjquery.com%2Ftutorial%2Four-team%2Fdemo40%2F&fwr=0&pra=3&rpe=1&resp_fmts=3&wgl=1&fa=40&uach=WyJXaW5kb3dzIiwiMTkuMC4wIiwieDg2IiwiIiwiMTI0LjAuNjM2Ny42MSIsbnVsbCwwLG51bGwsIjY0IixbWyJDaHJvbWl1bSIsIjEyNC4wLjYzNjcuNjEiXSxbIkdvb2dsZSBDaHJvbWUiLCIxMjQuMC42MzY3LjYxIl0sWyJOb3QtQS5CcmFuZCIsIjk5LjAuMC4wIl1dLDBd&dt=1714029748960&bpp=1&bdt=2360&idt=0&shv=r20240423&mjsv=m202404220101&ptt=9&saldr=aa&abxe=1&eoidce=1&prev_fmts=728x90%2C0x0%2C1200x90&nras=3&correlator=3449661086671&frm=20&pv=1&ga_vid=1790980417.1714029748&ga_sid=1714029748&ga_hid=429403528&ga_fc=0&u_tz=330&u_his=1&u_h=864&u_w=1536&u_ah=864&u_aw=1536&u_cd=24&u_sd=1.25&dmc=8&adx=20&ady=1710&biw=1519&bih=776&scr_x=0&scr_y=0&eid=44759875%2C44759926%2C44759837%2C95328447%2C31082778%2C31082968%2C42532524%2C95329831%2C95331045%2C31078663%2C31078665%2C31078668%2C31078670&oid=2&pvsid=4421459995527841&tmod=2067024898&uas=0&nvt=1&fc=896&brdim=0%2C0%2C0%2C0%2C1536%2C0%2C1536%2C864%2C1536%2C776&vis=1&rsz=%7C%7Cs%7C&abl=NS&fu=128&bc=31&bz=1&td=1&psd=W251bGwsbnVsbCxudWxsLDNd&nt=1&ifi=4&uci=a!4&btvi=2&fsb=1&dtd=12" data-google-container-id="a!4" tabindex="0" title="Advertisement" aria-label="Advertisement" data-google-query-id="CNGFhbPq3IUDFZ3hTAIdITYNsA" data-load-complete="true"></iframe></div></ins></div>
-   </div>
-  </div>
-
-  <a href="#" class="button button-primary mt-5">View all members</a>
- </div>
-</section>
-
-<section class="volunteer-section">
- <div class="container">
-  <h1>Be part of our community</h1>
-  <p>Join us in our mission to inspire and empower the next generation of innovators and problem solvers.</p>
-  <a href="#" class="button button-primary mt-3">Volunteer Form</a>
- </div>
-</section>
-
-
-<?php include 'includes/footer.php';?>
+<script>
+    $(document).ready(function() {
+        $('#link').click(function(event) {
+            event.preventDefault(); 
+            var url = $(this).attr('href'); 
+            $('body').fadeOut('slow', function() {
+                window.location.href = url; 
+            });
+        });
+    });
+</script>
+</body>
